ee832f18473290091203c51a60562ff8a3c71c19
Claudio Luppi <cluppi@redhat.com>
2020-05-26 14:21:35 +0100

    quarkus example completed

--- pam-quick-examples/pam-example-3/example3-parent/example3-kie-api-embedded-impl/src/main/java/com/redhat/cop/pam/example3/kie/api/impl/RulesApiImpl.java
+++ pam-quick-examples/pam-example-3/example3-parent/example3-kie-api-embedded-impl/src/main/java/com/redhat/cop/pam/example3/kie/api/impl/RulesApiImpl.java
@@ -5,22 +5,21 @@ import com.redhat.cop.pam.example3.Customer;
 import com.redhat.cop.pam.example3.kie.api.RulesApi;
 import com.redhat.cop.pam.example3.kie.api.utils.KieContainerUtils;
 import org.kie.api.runtime.KieContainer;
-import org.kie.dmn.api.core.*;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.jbpm.workflow.instance.WorkflowProcessInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApplicationScoped
 public class RulesApiImpl implements RulesApi {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RulesApiImpl.class);
-
-    private static final String NAMESPACE = "https://pam.cop.redhat.com/example3/dmn";
-
-    private static final String MODEL_NAME = "customer-can-open-account";
 
     @Inject
     KieContainerUtils kieContainerUtils;
@@ -28,23 +27,12 @@ public class RulesApiImpl implements RulesApi {
     @Override
     public CanOpenAccountResults canOpenAccount(final Customer customer) {
         final KieContainer kieContainer = kieContainerUtils.getKieContainer();
-        final DMNRuntime dmnRuntime = kieContainer.newKieSession().getKieRuntime(DMNRuntime.class);
-        final DMNModel dmnModel = dmnRuntime.getModel(NAMESPACE, MODEL_NAME);
-        final DMNContext dmnContext = dmnRuntime.newContext();
-        dmnContext.set("customer", customer);
-        final DMNResult dmnResult = dmnRuntime.evaluateAll(dmnModel, dmnContext);
-        return extractResult(dmnResult);
-    }
-
-    private CanOpenAccountResults extractResult(final DMNResult dmnResult) {
-        final DMNDecisionResult decisionResults = dmnResult.getDecisionResultByName("can open account");
-        if(decisionResults.hasErrors()){
-            final String errors = dmnResult.getMessages(DMNMessage.Severity.ERROR).stream()
-                    .map(message -> message.toString())
-                    .collect(Collectors.joining(", "));
-            throw new RuntimeException("DMN Error messages {" + errors + "}");
-        }
-        return CanOpenAccountResults.valueOf(String.valueOf(decisionResults.getResult()));
+        final Map<String, Object> processParameters = new HashMap<>();
+        processParameters.put("customerProcessVar", customer);
+        final KieSession kieSession = kieContainer.newKieSession();
+        final ProcessInstance processInstance  = kieSession.startProcess("example3-kjar.customer-can-open-account", processParameters);
+        final WorkflowProcessInstance workflowProcessInstance = (WorkflowProcessInstance) processInstance;
+        return CanOpenAccountResults.valueOf((String)workflowProcessInstance.getVariable("canOpenAccountProcessVar"));
     }
 
 }
