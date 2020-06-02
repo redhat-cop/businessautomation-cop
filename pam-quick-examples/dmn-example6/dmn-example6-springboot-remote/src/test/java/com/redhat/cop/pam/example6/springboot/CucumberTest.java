e49d9f9ddf18b39002caa4d7932adf9a929d379b
Claudio Luppi <cluppi@redhat.com>
2020-06-02 16:12:10 +0100

    example6 bdd test for DMN in process springboot remote

---
+++ pam-quick-examples/dmn-example6/dmn-example6-springboot-remote/src/test/java/com/redhat/cop/pam/example6/springboot/CucumberTest.java
@@ -0,0 +1,13 @@
+package com.redhat.cop.pam.example6.springboot;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features = "src/test/resources", //
+        plugin = { "html:target/cucumber", "json:target/cucumber.json" } )
+public class CucumberTest extends SpringConfiguration {
+
+}
