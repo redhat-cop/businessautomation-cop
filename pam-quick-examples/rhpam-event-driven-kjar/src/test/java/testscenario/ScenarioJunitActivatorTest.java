e35b44327f0ebeaedddb94decbf5edd0a1fc96a5
Donato Marrazzo <donato.marrazzo@gmail.com>
2020-12-16 19:31:44 +0100

    rhpam event driven demo

---
+++ pam-quick-examples/rhpam-event-driven-kjar/src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -0,0 +1,7 @@
+package testscenario;
+/**
+* Do not remove this file
+*/
+@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
+public class ScenarioJunitActivatorTest {
+}
