d6d160f97f5e1bc27c0f06dc4685717e8120ee8c
Stathis Rouvas <erouvas@redhat.com>
2021-07-25 02:50:16 +0100

    wip

---
+++ extras/kie_jwt_jaspi/dm_project/parent/src/main/java/com/bacop/jwt_dm_project/App.java
@@ -0,0 +1,13 @@
+package com.bacop.jwt_dm_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ extras/kie_jwt_jaspi/dm_project/rules/src/main/java/com/bacop/jwt_dm_project/ContinentMap.java
@@ -0,0 +1,63 @@
+package com.bacop.jwt_dm_project;
+
+/**
+ * ContinentMap: map Countries to Continents
+ */
+public class ContinentMap {
+
+  /**
+   * The name of the continent
+   */
+  String continent;
+
+  /**
+   * name of country
+   */
+  String country;
+
+  public ContinentMap() {
+    super();
+  }
+
+  public ContinentMap(String continent, String country) {
+    super();
+    this.continent = continent;
+    this.country = country;
+  }
+
+  /**
+   * @return the continent
+   */
+  public String getContinent() {
+    return continent;
+  }
+
+  /**
+   * @return the country
+   */
+  public String getCountry() {
+    return country;
+  }
+
+  /**
+   * @param continent the continent to set
+   */
+  public void setContinent(String continent) {
+    this.continent = continent;
+  }
+
+  /**
+   * @param country the country to set
+   */
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("ContinentMap [continent=").append(continent).append(", country=").append(country).append("]");
+    return builder.toString();
+  }
+
+}
